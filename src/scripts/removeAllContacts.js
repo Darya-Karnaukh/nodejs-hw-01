c4fc62644693e50f9a1dfe91a2f8eb7e506ed823
Darya-Karnaukh <153089841+Darya-Karnaukh@users.noreply.github.com>
2024-09-15 18:35:53 +0300

    fix-mentor

--- src/scripts/removeAllContacts.js
+++ src/scripts/removeAllContacts.js
@@ -2,7 +2,7 @@ import fs from 'node:fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
 
 export const removeAllContacts = async () => {
-  await fs.writeFile(PATH_DB, []);
+  await fs.writeFile(PATH_DB, JSON.stringify([], null, 2), 'utf-8');
 };
 
 removeAllContacts();
